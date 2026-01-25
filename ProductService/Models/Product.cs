1a5e39531e37810ad538d6c847e9a7b5d60b3d7e
Debjyoti1800 <debjyoti1800@gmail.com>
2026-01-26 00:24:22 +0530

    Made the user and product model.entity

---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductService.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Guid CategoryId { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string UserEmail { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTimeOffset DOB { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
